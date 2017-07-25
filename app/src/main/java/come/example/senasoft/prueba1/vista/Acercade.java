d413e85bc98beaec0adceb97b89d7b575b7b9cf9
Deivisosorio3 <deiviseguiso@gmail.com>
2017-07-25 18:31:10 +0200

    hjhk

---
+++ app/src/main/java/come/example/senasoft/prueba1/vista/Acercade.java
@@ -0,0 +1,15 @@
+package come.example.senasoft.prueba1.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import come.example.senasoft.prueba1.R;
+
+public class Acercade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acercade);
+    }
+}
